7e0d9a38153b1fa61c3434c84bb445dae589da68
Valtemir <valmartinsfilho@gmail.com>
2024-12-19 19:48:46 -0300

    alterando font e cores

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ type();
 window.addEventListener('scroll', function() {
   const navbar = document.querySelector('.navbar');
   if (window.scrollY > 50) {
-      navbar.style.background = 'rgba(255, 255, 255, 0.95)';
+      navbar.style.background = 'rgba(0, 0, 0, 0.95)';
       navbar.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.1)';
   } else {
       navbar.style.background = 'transparent';
